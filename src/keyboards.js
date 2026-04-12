3f0aa3c83e844da8ce3eac8a06b23a9a332f68e0
maximterentevasu-byte <maximterentevasu@gmail.com>
2026-04-12 23:42:54 +0500

    Delete src directory

--- src/keyboards.js
+++
@@ -1,9 +0,0 @@
-const { Markup } = require('telegraf');
-
-function mainKeyboard() {
-  return Markup.keyboard([
-    ['Загрузить файл', 'Проверить ШК']
-  ]).resize();
-}
-
-module.exports = { mainKeyboard };
